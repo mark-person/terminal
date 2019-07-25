6daa721f48ad8493a7cf0bd74f83f59d74659dcb
dengxz <6882855@qq.com>
2019-07-25 12:16:54 +0800

    t

--- server/src/main/resources/static/common/common-0.1.0.js
+++ server/src/main/resources/static/common/common-0.1.0.js
@@ -1,2 +1,15 @@
 
 
+$.ajaxSetup({
+	error: function(r, textStatus, errorThrown) {
+		alert("error:" + r.status + "|" + this.url);
+	},
+	complete: function(r, textStatus, errorThrown) {
+	    var code = JSON.parse(r.responseText).code;
+		if (code != 0) {
+			alert(r.responseText);
+		}
+	}
+});
+
+
