c749849cfecbf640e0500046056834348d3b837d
mark <13602493280@139.com>
2019-08-16 23:35:36 +0800

    t

--- server/src/main/resources/static/vue/template/modal.vue
+++ server/src/main/resources/static/vue/template/modal.vue
@@ -3,12 +3,12 @@
 <template>
 <div class="modal-mask">
 <div class="modal-wrapper">
-	<div class="modal-container" :style="'width:'+width">
-		<div class="modal-header"><h3>{{title}}</h3></div>
+	<div class="modal-container" :style="'width:'+modal.width">
+		<div class="modal-header"><h3>{{modal.title}}</h3></div>
 		<div class="modal-body"><slot name="body"></slot></div>
 		<div class="modal-footer">&nbsp;
 			<div class="modal-default-button">
-				<button v-if="showOk" @click="modal.ok(param)" style="border:1px solid blue">确定</button>
+				<button v-if="modal.showOk" @click="modal.ok(param)" style="border:1px solid blue">确定</button>
 				<button @click="modal.showModal = false">关闭</button>
 			</div>
 		</div>
@@ -20,13 +20,6 @@
 
 <script>
 module.exports = {
-	props: {modal:Object,param:Object},
-    data: function() {
-        return {
-            title:this.modal.title,
-            width:this.modal.width,
-            showOk:this.modal.showOk
-        }
-    }
+	props: {modal:Object,param:Object}
 }
 </script>
