164fe003692daf298b18edccfa73979676a5b6c7
dengxz <6882855@qq.com>
2019-08-19 17:00:47 +0800

    t

--- server/src/main/resources/static/vue/template/modal.vue
+++ server/src/main/resources/static/vue/template/modal.vue
@@ -8,8 +8,8 @@
 		<div class="modal-body"><slot name="body"></slot></div>
 		<div class="modal-footer">&nbsp;
 			<div class="modal-default-button">
-				<button v-if="modal.showOk" @click="modal.ok(param)" style="border:1px solid blue">确定</button>
-				<button @click="modal.showModal = false">关闭</button>
+				<button class="v-button" v-if="modal.showOk" @click="modal.ok(param)" style="border:1px solid blue">确定</button>
+				<button class="v-button" @click="modal.showModal = false">关闭</button>
 			</div>
 		</div>
 	</div>
