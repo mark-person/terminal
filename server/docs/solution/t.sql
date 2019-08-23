3cc2a916bb1549c78fc5098b7279d0ce4e0a23fc
dengxz <6882855@qq.com>
2019-08-23 14:32:24 +0800

    t

--- server/docs/solution/t.sql
+++ server/docs/solution/t.sql
@@ -10,3 +10,19 @@ create table test_cart (
 select t.* from (
 select test_cart.*, @r := (@minus := sku_num + @minus) - @minus_num n, if(@r <= 0, 0, if(sku_num - @r <= 0, sku_num, @r)) real_num from test_cart, 
 (select @minus := 0) t0, (select @minus_num := sum(sku_num) div 4 from test_cart) t1 order by sku_price) t
+
+
+select t.*, @group_sum := group_sum,
+case @group_id := t.group_id
+when 100 then
+(
+select sum(sku_price * real_num) price from (
+select t.*, @r := (@minus := sku_num + @minus) - @group_sum n, if(@r <= 0, 0, if(sku_num - @r <= 0, sku_num, @r)) real_num 
+from (select * from test_cart where group_id = @group_id) t, 
+(select @minus := 0) t0 order by sku_price) t
+) 
+else -1 end D
+from (select c.group_id, sum(c.sku_num) div 4 group_sum from test_cart c group by c.group_id) t
+
+
+
