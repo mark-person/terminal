e7674d729a0e06df57a8aebc851ce2c7e521da06
dengxz <6882855@qq.com>
2019-09-02 17:31:36 +0800

    t

--- server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqController.java
+++ server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqController.java
@@ -31,71 +31,16 @@ public class FaqController {
 	
 	@Autowired
 	private FaqImpl impl;
-	
-	@GetMapping("/faqTest")
-	public ModelAndView faqTest(ModelAndView mv) {
-		
-		
-		
-		mv.setViewName("app/core/faq/faqTest");
-		return mv;
-	}
-	
-	@GetMapping("/js")
-	public void js(HttpServletResponse response) {
-		
-		/*
-		 3.max-age 指定的是从文档被访问后的存活时间，这个时间是个相对值(比如:3600s),相对的是文档第一次被请求时服务器记录的Request_time(请求时间)
-		 */
-		// response.setHeader("Expires", new Date(new Date().getTime() + 5000).toGMTString());
-		// response.setHeader("Expires", "Sun, 01 Sep 2019 06:56:33 GMT");
-		response.setHeader("content-type", "application/javascript");
-		response.setHeader("cache-control", "max-age=10");
-		
-		try {
-			System.out.println("99999999999");
-			
-			response.getWriter().write("");
-			response.flushBuffer();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-	}
-	
+
 	@GetMapping("/faqIndex")
 	public ModelAndView faqIndex(ModelAndView mv) {
 		mv.setViewName("app/core/faq/faqIndex");
-		mv.addObject("list", list(new Page(), new Faq()));
-		
-		// 的sql里转JSON_OBJECT
-		String s = "{\"B\":1,\"A\":2}";
-		mv.addObject("faqCategory", JSONObject.fromObject(s));
-		
-		System.out.println("99999999:" + JSONObject.fromObject(s));
-		
+		mv.addObject("list", list(new Page(), new Faq()));	
 		return mv;
 	}
 	
 	@PostMapping("/list") @ResponseBody
 	public Map<String, Object> list(Page page, Faq pojo) {
-		
-		
-		try {
-			String jsonStr = "{brain:'头脑风暴',tech:'技术',busi:'业务'}";
-			
-			
-			JSONObject jsonObject = JSONObject.fromObject(jsonStr);
-			// JsonNode jn = new ObjectMapper().readTree(jsonStr);
-			List<String> list = IteratorUtils.toList(jsonObject.keys());
-			
-			
-			System.out.println("9999999:");
-			System.out.println("..........jn:" + list);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		
 		List<Faq> list = impl.list(page, pojo);
 		return ControllerReturn.page(page, list);
