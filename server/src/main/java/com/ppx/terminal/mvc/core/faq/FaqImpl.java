98ff86e2f8923af7f86de52d26a771a442f2bb6f
mark <13602493280@139.com>
2019-09-04 21:56:40 +0800

    t

--- server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqImpl.java
+++ server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqImpl.java
@@ -25,13 +25,6 @@ public class FaqImpl extends MyDaoSupport {
 	@Transactional
 	public List<Faq> list(Page page, Faq pojo) {
 		
-		// >>>>>>>>..test
-		String sql = "select @j:=JSON_OBJECT('1','OK1', '2','NB')";
-		getJdbcTemplate().queryForMap(sql);
-		sql = "select test_id, JSON_UNQUOTE(JSON_EXTRACT(@j, concat('$.\"', test_name, '\"'))) n from test";
-		List<Map<String, Object>> testList = getJdbcTemplate().queryForList(sql);
-		System.out.println("xxxxx:" + testList.size());
-		
 		
 		MyCriteria c = createCriteria("where")
 			.addAnd("t.faq_title like ?", "%", pojo.getFaqTitle(), "%");
