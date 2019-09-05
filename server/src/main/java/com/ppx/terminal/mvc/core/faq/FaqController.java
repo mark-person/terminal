279d192aaa9d2e4fa463876b007656e5286f581d
dengxz <6882855@qq.com>
2019-09-05 15:27:02 +0800

    t

--- server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqController.java
+++ server/src/main/java/com/ppx/terminal/mvc/core/faq/FaqController.java
@@ -1,15 +1,11 @@
 package com.ppx.terminal.mvc.core.faq;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +16,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.ppx.terminal.common.controller.ControllerReturn;
 import com.ppx.terminal.common.page.Page;
-
-import net.sf.json.JSONObject;
 
 
 
